1f1e5d24bd8e08e65dd73f770f88a1d99e4b8a3c
Alex <alex@a.de>
2022-06-01 20:19:56 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+console.log('Client side javascript is loaded')
+
+
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+let messageOne = document.querySelector('#message-1');
+let messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne = 'Error';
+                console.log(data.error)
+            } else {
+                console.log(data.location);
+                console.log(data.forecast)
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    })
+});
