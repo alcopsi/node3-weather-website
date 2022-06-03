c093301cd164c4d9281dacdc35275572941cdc09
Alex <alex@a.de>
2022-06-03 21:03:50 +0200

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request');
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=7ddbb99b08bac73f3e1d2d40557fb940&query=${latitude},${longitude}`;
 
@@ -10,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         }
         else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees.`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`);
         }
     })
 }
